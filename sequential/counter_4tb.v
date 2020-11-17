488ee9424c5b71d270345fd3ba209c64977127ce
Nihal Ramaswamy <41371273+nihal-ramaswamy@users.noreply.github.com>
2020-11-17 16:31:47 +0530

    checked for plagiarism

--- sequential/counter_4tb.v
+++ sequential/counter_4tb.v
@@ -1,35 +1,35 @@
 module tb_counter();
 
-reg clock,up_down,reset;
-wire [3:0]q;
-counter c4(clock, reset,up_down , q);
+reg clock_signal,up_down,reset_signal;
+wire [3:0]output_wire;
+counter c4(clock_signal, reset_signal,up_down , output_wire);
 initial 
 	begin 
 		$dumpfile("tb_counter_dump.vcd"); 
 		$dumpvars(0,tb_counter); 
 	end
 initial 
-begin clock = 1'b0; repeat(60) #10 clock =~ clock;
+begin clock_signal = 1'b0; repeat(60) #10 clock_signal =~ clock_signal;
 end
 
 initial 
 begin
-	reset=1'b0;
+	reset_signal=1'b0;
 	up_down=1'b0;
 	#10
-	reset=1'b1;
+	reset_signal=1'b1;
 	#10;
-	reset=1'b0;
+	reset_signal=1'b0;
 	up_down=1'b0;
 	#90
-	reset=1'b0;
+	reset_signal=1'b0;
 	up_down=1'b1;
 	#50
-	reset=1'b1;
+	reset_signal=1'b1;
 	#20
-	reset=1'b0;
+	reset_signal=1'b0;
 	#250;
-	reset=1'b0;
+	reset_signal=1'b0;
 	up_down=1'b0;
 end
 
